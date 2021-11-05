26579b7a5eb9cc2a8412ed017f1add6c0c15d635
SebHW <93712019+SebHW@users.noreply.github.com>
2021-11-05 10:48:32 +0000

    Add files via upload

---
+++ Problem 10 Next Prime Number/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_10_Next_Prime_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input the number you wish to find the next prime number from");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(number);
+            Console.WriteLine(number);
+            Console.WriteLine(number);
+            number++;
+            while (IsPrime(number) == false)
+            {
+                number++;
+            }
+            Console.WriteLine("{0} is the next prime number up from the one you inputted",number);
+            Console.ReadLine();
+        }
+        public static Boolean IsPrime(int x)
+        {
+            if (x <= 1)
+            {
+                return false;
+            }
+            if (x <= 3)
+            {
+                return true;
+            }
+            if (x % 2 == 0 || x % 3 == 0)
+            {
+                return false;
+            }
+            else
+            {
+                for(int i = 5; i <= Math.Sqrt(x); i++)
+                {
+                    if (x % i == 0 || x % (i+2) == 0)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            
+        }
+    }
+}
---
+++ Problem 9 Prime at Nth Pos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_9_Prime_at_Nth_Pos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string primes = "2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97";
+            string[] primeArray = primes.Split(',');
+            Console.WriteLine("Please input the prime number you wish to find the next one up from");
+            string prime = Console.ReadLine();
+            int index = Array.IndexOf(primeArray, prime);
+            Console.WriteLine("The next prime is {0}",primeArray[index + 1]);
+            Console.ReadLine();
+        }
+    }
+}
